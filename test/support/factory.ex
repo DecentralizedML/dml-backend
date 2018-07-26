b5596eeb9b6a9a6e3d5e12511a29f9cfe5c00dbf
Thiago Belem <tbelem@kyokan.io>
2018-07-26 14:40:05 -0300

    Create Accounts context with User model

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,3 +1,19 @@
 defmodule Dml.Factory do
   use ExMachina.Ecto, repo: Dml.Repo
+
+  def user_factory do
+    email = random_string(10)
+
+    %Dml.Accounts.User{
+      email: "user_#{email}@kyokan.io",
+      password: random_string(10)
+    }
+  end
+
+  defp random_string(length) do
+    length
+    |> :crypto.strong_rand_bytes
+    |> Base.url_encode64
+    |> binary_part(0, length)
+  end
 end
