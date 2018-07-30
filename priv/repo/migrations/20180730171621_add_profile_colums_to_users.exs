e3d9d525b0aeca3b9879af734d9f55aa8ec3af26
Thiago Belem <tbelem@kyokan.io>
2018-07-30 14:47:43 -0300

    Add first name, last name & bio to users

---
+++ priv/repo/migrations/20180730171621_add_profile_colums_to_users.exs
@@ -0,0 +1,11 @@
+defmodule Dml.Repo.Migrations.AddProfileColumsToUsers do
+  use Ecto.Migration
+
+  def change do
+    alter table(:users) do
+      add :first_name, :string
+      add :last_name, :string
+      add :bio, :text, null: true
+    end
+  end
+end
