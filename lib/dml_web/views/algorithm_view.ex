238d91ea9dccdd94a0b15a55aa231037b8dd12d7
Thiago Belem <tbelem@kyokan.io>
2018-09-24 13:02:10 -0300

    Add algorithm creation endpoints

---
+++ lib/dml_web/views/algorithm_view.ex
@@ -0,0 +1,23 @@
+defmodule DmlWeb.AlgorithmView do
+  use DmlWeb, :view
+  alias DmlWeb.{AlgorithmView, UserView}
+
+  def render("index.json", %{algorithms: algorithms}) do
+    render_many(algorithms, AlgorithmView, "algorithm.json")
+  end
+
+  def render("show.json", %{algorithm: algorithm}) do
+    render_one(algorithm, AlgorithmView, "algorithm.json")
+  end
+
+  def render("algorithm.json", %{algorithm: algorithm}) do
+    %{
+      id: algorithm.id,
+      title: algorithm.title,
+      description: algorithm.description,
+      device_fee: algorithm.device_fee,
+      state: algorithm.state,
+      user: UserView.render("user.json", %{user: algorithm.user})
+    }
+  end
+end
