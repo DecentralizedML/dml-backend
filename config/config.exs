30922b04064ea84185241ed79568d0d3d418877d
Thiago Belem <tbelem@kyokan.io>
2018-11-22 16:48:10 -0200

    Add fields to user JSON

--- config/config.exs
+++ config/config.exs
@@ -52,6 +52,10 @@ config :sentry,
   enable_source_code_context: true,
   root_source_code_path: File.cwd!()
 
+# JSONAPI
+config :jsonapi,
+  remove_links: true
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
