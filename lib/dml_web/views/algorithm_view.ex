fe104a5e88b2af813a9fd681ba2272e98f1ceae9
Thiago Belem <tbelem@kyokan.io>
2018-11-23 15:57:29 -0200

    Add tags to Algorithm

--- lib/dml_web/views/algorithm_view.ex
+++ lib/dml_web/views/algorithm_view.ex
@@ -2,7 +2,7 @@ defmodule DmlWeb.AlgorithmView do
   use JSONAPI.View, type: "algorithms", namespace: "/api"
 
   def fields do
-    [:title, :description, :device_fee, :state]
+    [:title, :description, :device_fee, :tags, :state]
   end
 
   def relationships do
